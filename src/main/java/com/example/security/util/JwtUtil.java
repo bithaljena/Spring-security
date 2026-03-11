7e308c52e65628cae8615f200eba09a5ce6414fb
bithaljena <bithaljena1999@gmail.com>
2026-03-11 07:18:02 +0530

    second commit

--- src/main/java/com/example/security/util/JwtUtil.java
+++ src/main/java/com/example/security/util/JwtUtil.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "mysecretmysecretmysecretmysecret";
+    private final String SECRET = "itshould_be_dynamic_value";
 
     private Key key() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
