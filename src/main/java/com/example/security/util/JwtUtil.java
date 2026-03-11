30a028317951aa16a2363ef653b05c32f809fda8
bithaljena <bithaljena1999@gmail.com>
2026-03-11 06:57:12 +0530

    Initial commit

---
+++ src/main/java/com/example/security/util/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.example.security.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "mysecretmysecretmysecretmysecret";
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+86400000))
+                .signWith(key())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validate(String token) {
+        try{
+            Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJws(token);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+}
